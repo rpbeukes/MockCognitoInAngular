e3df54edb387ca29f4e3c31a60382e88d9fa3640
Ruan Beukes <ruan.coder@gmail.com>
2019-10-24 12:18:19 +0800

    example match angular test

--- node-property-descriptor-investigation/test.spec.js
+++ node-property-descriptor-investigation/test.spec.js
@@ -1,19 +1,19 @@
-const cognito = require('amazon-cognito-identity-js');
+const AWSCognito = require('amazon-cognito-identity-js');
 
-describe('Investigaste Object.defineProperty of Cognito.AuthenticationDetails', () => {
+describe('Investigaste Object.defineProperty of AWSCognito.AuthenticationDetails', () => {
   it('It should throw "TypeError: Cannot redefine property: AuthenticationDetails" because Object.getOwnPropertyDescriptor returns configurable: false', () => {
     
-    console.log('Object.getOwnPropertyDescriptor(cognito, "AuthenticationDetails"): ');
-    console.log(Object.getOwnPropertyDescriptor(cognito, 'AuthenticationDetails'));
+    console.log('Object.getOwnPropertyDescriptor(AWSCognito, "AuthenticationDetails"): ');
+    console.log(Object.getOwnPropertyDescriptor(AWSCognito, 'AuthenticationDetails'));
 
     // this will throw exception because of 'configurable: false',
     // meaning the AWS team does not want you to fiddle with this...and I agree, you shouldn't.
-    Object.defineProperty(cognito, 'AuthenticationDetails', {
+    Object.defineProperty(AWSCognito, 'AuthenticationDetails', {
       writable: true,
       value: 'foo'
     });
     
-    console.log(Object.getOwnPropertyDescriptor(cognito, 'AuthenticationDetails'));
+    console.log(Object.getOwnPropertyDescriptor(AWSCognito, 'AuthenticationDetails'));
   });
 
 });
