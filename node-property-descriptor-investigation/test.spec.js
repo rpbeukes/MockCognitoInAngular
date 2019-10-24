3ad413acbc0c89261a22994f973fc6b29c292c62
Ruan Beukes <ruan.coder@gmail.com>
2019-10-24 12:14:21 +0800

    add some description to the test

--- node-property-descriptor-investigation/test.spec.js
+++ node-property-descriptor-investigation/test.spec.js
@@ -1,13 +1,19 @@
 const cognito = require('amazon-cognito-identity-js');
-describe('FML', () => {
-  it('Works', () => {
-    console.log(Object.getOwnPropertyDescriptor(cognito, 'CognitoUserPool'));
 
-    Object.defineProperty(cognito, 'CognitoUserPool', {
-      value: 'foo',
-      writable: false
+describe('Investigaste Object.defineProperty of Cognito.AuthenticationDetails', () => {
+  it('It should throw "TypeError: Cannot redefine property: AuthenticationDetails" because Object.getOwnPropertyDescriptor return configurable: false', () => {
+    
+    console.log('Object.getOwnPropertyDescriptor(cognito, "AuthenticationDetails"): ');
+    console.log(Object.getOwnPropertyDescriptor(cognito, 'AuthenticationDetails'));
+
+    // this will throw exception because of 'configurable: false',
+    // meaning the AWS team does not want you to fiddle with this...and I agree, you shouldn't.
+    Object.defineProperty(cognito, 'AuthenticationDetails', {
+      writable: true,
+      value: 'foo'
     });
     
-    console.log(Object.getOwnPropertyDescriptor(cognito, 'CognitoUserPool'));
+    console.log(Object.getOwnPropertyDescriptor(cognito, 'AuthenticationDetails'));
   });
+
 });
