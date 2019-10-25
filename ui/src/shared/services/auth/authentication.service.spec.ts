af24089fa8c6afbf1ecf9b6f741058d1dc1f0a6d
Ruan Beukes <ruan.coder@gmail.com>
2019-10-25 10:45:05 +0800

    move complete test to separate file

--- ui/src/shared/services/auth/authentication.service.spec.ts
+++ ui/src/shared/services/auth/authentication.service.spec.ts
@@ -3,7 +3,11 @@ import * as AWSCognito from 'amazon-cognito-identity-js';
 import { AuthenticationService } from './authentication.service';
 import { environment } from 'src/environments/environment';
 
-fdescribe('AuthenticationService', () => {
+ /*
+  see authentication.service.complete.spec.ts for complete solution
+ */
+
+describe('AuthenticationServiceAuthenticationDetailsFocused', () => {
   let service: AuthenticationService;
 
   beforeEach(() => {
@@ -12,12 +16,11 @@ fdescribe('AuthenticationService', () => {
     service = TestBed.get(AuthenticationService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-
   describe('when .signInUser is working normally', () => {
-    let spyAuthenticationDetails;
+    let spyAuthenticationDetails: jasmine.Spy;
+    let spyCognitoUser: jasmine.Spy;
+    let spyCognitoUserPool: jasmine.Spy;
+
     let result;
 
     beforeEach(async () => {
@@ -34,8 +37,8 @@ fdescribe('AuthenticationService', () => {
                 at runInTestZone (http://localhost:9876/_karma_webpack_/node_modules/zone.js/dist/zone-testing.js:561:1)
        */
 
-      // why does this output `configurable: true` in Angular world but not in Node???
-      //      Object{get: function() { ... }, set: undefined, enumerable: true, configurable: true} 
+      // why does this output `configurable: true` in Angular world but in node.js `configurable: false`???
+      //      output: Object{get: function() { ... }, set: undefined, enumerable: true, configurable: true} 
       console.log(Object.getOwnPropertyDescriptor(AWSCognito, 'AuthenticationDetails'));
 
       // arrange
@@ -46,13 +49,19 @@ fdescribe('AuthenticationService', () => {
 
       spyAuthenticationDetails = spyOn(AWSCognito, 'AuthenticationDetails');
 
+      // create the rest of the cognito spies needed just to get AuthenticationDetails tested.
+      // in the authentication.service.complete.spec.ts, the spy of AuthenticationDetails also lives
+      // inside the createSpies() function.
+      // it is outside the function to show intent with this particular test.
+      createSpies();
+
       // act
       service.signInUser({ userName: 'hello', password: 'password' }).subscribe(
         value => {
           result = value;
         },
         error => {
-          throw Error(error);
+          console.log(error);
         }
       );
     });
@@ -66,136 +75,18 @@ fdescribe('AuthenticationService', () => {
       });
     });
 
-    xit('instantiate CognitoUser with correct CognitoUserData', async () => {
-      fail('ToDo');
-    });
+    function createSpies() {
+      defineWriteable(AWSCognito, 'CognitoUserPool');
+      defineWriteable(AWSCognito, 'CognitoUser');
+      spyCognitoUserPool = spyOn(AWSCognito, 'CognitoUserPool');
+      spyCognitoUser = spyOn(AWSCognito, 'CognitoUser');
+    }
 
-    xit('it should return the username', () => {
-      fail('ToDo');
-    });
+    function defineWriteable(obj: any, functionName: string) {
+      Object.defineProperty(obj, functionName, {
+        writable: true,
+        value: 'foo'
+      });
+    }
   });
 });
-
-xdescribe('AuthenticationService', () => {
-  // backup environment values
-  const envBackup = JSON.stringify(environment);
-  let service: AuthenticationService;
-  let spyAuthenticationDetails: jasmine.Spy;
-  let spyCognitoUser: jasmine.Spy;
-  let spyCognitoUserPool: jasmine.Spy;
-  let result: { userName: string };
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    createSpies();
-    environment.userPoolData.userPoolId = 'pool-id';
-    environment.userPoolData.appClientId = 'app-client-id';
-  });
-
-  afterEach(() => {
-    // restore environment values
-    Object.assign(environment, JSON.parse(envBackup));
-  });
-
-  it('should be created', () => {
-    service = TestBed.get(AuthenticationService);
-    expect(service).toBeTruthy();
-  });
-
-  describe('when .signInUser is working normally', () => {
-    let spyCognitoUserArgs;
-    beforeEach(async () => {
-      const fakeCognitoUser = jasmine.createSpyObj<AWSCognito.CognitoUser>([
-        'authenticateUser'
-      ]);
-      fakeCognitoUser.authenticateUser.and.callFake(
-        (
-          authDetails: AWSCognito.AuthenticationDetails,
-          callback: AWSCognito.IAuthenticationCallback
-        ) => {
-          callback.onSuccess(createFakeCognitoUserSession());
-        }
-      );
-
-      spyCognitoUser.and.callFake(args => {
-        spyCognitoUserArgs = args;
-        return fakeCognitoUser;
-      });
-
-      service = TestBed.get(AuthenticationService);
-
-      // act
-      service.signInUser({ userName: 'hello', password: 'password' }).subscribe(
-        value => {
-          result = value;
-        },
-        error => {
-          throw Error(error);
-        }
-      );
-    });
-
-    it('instantiate cognito AuthenticationDetails with correct user credentials', () => {
-      expect(spyAuthenticationDetails).toHaveBeenCalledTimes(1);
-      expect(spyAuthenticationDetails).toHaveBeenCalledWith({
-        Username: 'hello',
-        Password: 'password'
-      });
-    });
-
-    it('instantiate CognitoUser with correct CognitoUserData', async () => {
-      expect(spyCognitoUser).toHaveBeenCalledTimes(1);
-      expect(spyCognitoUserArgs.Username).toBe('hello');
-      expect(spyCognitoUserPool).toHaveBeenCalledTimes(1);
-      expect(spyCognitoUserPool).toHaveBeenCalledWith({
-        UserPoolId: 'pool-id',
-        ClientId: 'app-client-id'
-      });
-    });
-
-    it('it should return the username', () => {
-      expect(result).toEqual({ userName: 'hello' });
-    });
-  });
-
-  function createSpies() {
-    console.log(Object.getOwnPropertyDescriptor(AWSCognito, 'CognitoUserPool'));
-    defineWriteable(AWSCognito, 'CognitoUserPool');
-    defineWriteable(AWSCognito, 'AuthenticationDetails');
-    defineWriteable(AWSCognito, 'CognitoUser');
-    spyCognitoUserPool = spyOn(AWSCognito, 'CognitoUserPool');
-    spyAuthenticationDetails = spyOn(AWSCognito, 'AuthenticationDetails');
-    spyCognitoUser = spyOn(AWSCognito, 'CognitoUser');
-  }
-});
-
-function defineWriteable(obj: any, functionName: string) {
-  Object.defineProperty(obj, functionName, {
-    writable: true,
-    value: something => {}
-  });
-}
-
-function createFakeCognitoUserSession() {
-  const fakeCognitoUserSession = jasmine.createSpyObj<
-    AWSCognito.CognitoUserSession
-  >(['getIdToken', 'getAccessToken']);
-  fakeCognitoUserSession.getIdToken.and.callFake(() => {
-    const fakeToken = new AWSCognito.CognitoIdToken({
-      IdToken: 'some token value'
-    });
-    fakeToken.payload = {
-      email: 'hello@email.com',
-      'cognito:username': 'hello',
-      sub: 'user-database-Id'
-    };
-    return fakeToken;
-  });
-  fakeCognitoUserSession.getAccessToken.and.callFake(() => {
-    const fakeToken = new AWSCognito.CognitoAccessToken({
-      AccessToken: 'some AcccessToken value'
-    });
-    return fakeToken;
-  });
-  return fakeCognitoUserSession;
-}
